5e90ed55e2c39855f4de5e5b365d80dd4e94ef89
krisquigley <kris@krisquigley.co.uk>
2017-02-18 16:10:45 +0000

    Finished basic styling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,21 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <Logo />
         <div className='c-article'>
-          <p>This is a sentence</p>
+          <image src={Logo} />
+          <div className='c-article__category'>
+            <span className='c-article__category_logo'>≡</span>
+          </div>
+          <hr className='c-article__rule' />
+          <span className='c-article__author'>
+            Dave Smith
+          </span>
+          <span className='c-article__read_count'>
+            231 <i>reads</i>
+          </span>
+          <h2 className='c-article__title'>
+            Instagram is fuelling food waste
+          </h2>
           <div className='c-article__read_more'>
             Read Article <span className='c-article__arrow'>→</span>
           </div>
