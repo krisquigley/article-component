39cc692fde2ef1333e9654c2ed50f425ff7b5350
krisquigley <kris@krisquigley.co.uk>
2017-02-18 16:29:26 +0000

    Refactored

--- src/components/ArticlePanel.js
+++ src/components/ArticlePanel.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
-import './App.css'
-import './components/ArticlePanel.css'
-import Logo from './logo.svg'
+import './ArticlePanel.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
+      <div>
         <div className='c-article'>
-          <image src={Logo} />
           <div className='c-article__category'>
             <span className='c-article__category_logo'>≡</span>
           </div>
@@ -17,14 +14,16 @@ class App extends Component {
             Dave Smith
           </span>
           <span className='c-article__read_count'>
-            231 <i>reads</i>
+            342 <i>reads</i>
           </span>
           <h2 className='c-article__title'>
             Instagram is fuelling food waste
           </h2>
-          <div className='c-article__read_more'>
-            Read Article <span className='c-article__arrow'>→</span>
-          </div>
+          <a href='/'>
+            <div className='c-article__read_more'>
+              Read Article <span className='c-article__arrow'>→</span>
+            </div>
+          </a>
         </div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ArticlePanel from './components/ArticlePanel'
+import './index.css'
 
 ReactDOM.render(
-  <App />,
+  <ArticlePanel />,
   document.getElementById('root')
-);
+)
